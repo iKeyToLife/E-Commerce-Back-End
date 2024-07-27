4ffdc4e0d307b8e38dc282f719128f16e48683bf
Aleksandr <keytolife94@gmail.com>
2024-07-27 01:02:43 -0500

    complete delete post put methods for api/,,,

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,12 +36,37 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  /* req.body should look like this...
+    {
+      "category_name": "Sea Food"
+    }
+  */
+  try {
+    const categoriesData = await Category.create(req.body);
+
+    res.status(200).json(categoriesData);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoriesData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(500).json(categoriesData);
+  } catch (e) {
+    res.status(500).json(e)
+  }
+
 });
 
 router.delete('/:id', async (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,12 +36,37 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  /* req.body should look like this...
+    {
+      "tag_name": "Sea Food"
+    }
+  */
+  try {
+    const tagsData = await Tag.create(req.body);
+
+    res.status(200).json(tagsData);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagsData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(500).json(tagsData);
+  } catch (e) {
+    res.status(500).json(e)
+  }
+
 });
 
 router.delete('/:id', async (req, res) => {
